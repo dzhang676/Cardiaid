670b3d6a79b95d6e4f5d55a8a71f24b403c78eeb
n3dim <nedimhodzic42@gmail.com>
2019-03-11 17:49:02 -0400

    setting up routes

--- frontend/cardiaid_ui/src/App.js
+++ frontend/cardiaid_ui/src/App.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Routes from "./Routes";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { test: "hello" };
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Routes test={this.state.test} />;
   }
 }
 
