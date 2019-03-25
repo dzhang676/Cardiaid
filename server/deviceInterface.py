cc5c2c1bec8027afa4a1e9478ce8a9db2bb41898
David Zhang <dbzhang@uwaterloo.ca>
2019-03-25 16:19:19 -0400

    implemented bluetooth connection

--- server/deviceInterface.py
+++ server/deviceInterface.py
@@ -1,45 +1,62 @@
-import serial
 import datetime
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+import bluetooth
+import time
 
 class ArduinoInterface():
-    #ser = serial.Serial('COM3') # use COM3 for now until bluetooth connection 
-    #ser.baudrate = 9600
-    #ser.open()
+    nearby_devices = bluetooth.discover_devices()
+    bd_addr = "00:14:03:06:8D:0F"
+    sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
     data = {"HRValid": True, "BOValid": True, "HR": 0, "BO":0}
     HRcache = []
     BOcache = []
+    port = 1
+    print("trying to connect")
+    sock.connect((bd_addr, port))
+    print("connected")
+
     def __init__(self):
         return
 
     def returnVitals(self):
         # red=0, ir=0, HR=-999, HRvalid=0, SPO2=-999, SPO2Valid=0
-        curr = "red=0, ir=0, HR=999, HRvalid=1, SPO2=88, SPO2Valid=1".split(',') # ser.read_until().split(',')
-        if curr[-1].split('=')[1] == 0:
-            self.data["BOValid"] = False
-            self.BOcache.clear()
-        else:
-            self.data["BOValid"] = True
-            self.BOcache.append(float(curr[-2].split('=')[1]))
-        if curr[-3].split('=')[1] == 0:
-            self.data["HRValid"] = False
-            self.HRcache.clear()
-        else:
-            self.data["HRValid"] = True
-            self.HRcache.append(float(curr[2].split('=')[1]))
+        curr = None
+        readings = str(self.sock.recv(100)).split("\\r\\n")
+        time.sleep(0.5)
+        for reading in readings:
+            if reading[:3] == "red":
+                curr = reading.split(',')
+                break
+                
+        print(readings)
+        print("curr:")
+        print(curr)
+        if curr is not None and curr[0][:3] == "red" and curr[-1][1:10] == 'SPO2Valid':
+            if curr[-1].split('=')[1] == 0:
+                self.data["BOValid"] = False
+                self.BOcache.clear()
+            else:
+                self.data["BOValid"] = True
+                self.BOcache.append(float(curr[-2].split('=')[1]))
+            if curr[-3].split('=')[1] == 0:
+                self.data["HRValid"] = False
+                self.HRcache.clear()
+            else:
+                self.data["HRValid"] = True
+                self.HRcache.append(float(curr[2].split('=')[1]))
 
-        if len(self.HRcache) >= 500:
-            avgHR = sum(self.HRcache) // 500
-            self.HRcache.clear()
-            self.data["HR"] = avgHR
+            if len(self.HRcache) >= 10:
+                avgHR = sum(self.HRcache) // 10
+                self.HRcache.clear()
+                self.data["HR"] = avgHR
 
-        if len(self.BOcache) >= 500:
-            avgBO = sum(self.BOcache) // 500
-            self.BOcache.clear()
-            self.data["BO"] = avgBO
+            if len(self.BOcache) >= 10:
+                avgBO = sum(self.BOcache) // 10
+                self.BOcache.clear()
+                self.data["BO"] = avgBO
         
         return self.data
 
